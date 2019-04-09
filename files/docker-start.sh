15b5015d0c0ecfa14f32249811db2cdc7fda7474
Rp70 <14834373+Rp70@users.noreply.github.com>
2019-04-09 07:12:38 +0700

    commit

--- files/docker-start.sh
+++ files/docker-start.sh
@@ -7,6 +7,7 @@ if [ "$1" = 'startup' ]; then
 	CRON_COMMANDS=${CRON_COMMANDS:=''}
 	MEMCACHED_ENABLE=${MEMCACHED_ENABLE:=''}
 	NGINX_ENABLE=${NGINX_ENABLE:=''}
+	NGINX_PROCESSES=${NGINX_PROCESSES:='2'}
 	SUPERVISOR_ENABLE=0
 
 	if [ "$CRON_COMMANDS" != '' ]; then
@@ -32,7 +33,7 @@ if [ "$1" = 'startup' ]; then
 		rm -rf /etc/nginx/sites-available/default
 		#sed -i 's/^user/daemon off;\nuser/g' /etc/nginx/nginx.conf
 		#sed -i 's/^user www-data;/user coin;/g' /etc/nginx/nginx.conf
-		sed -i 's/^worker_processes auto;/worker_processes 2;/g' /etc/nginx/nginx.conf
+		sed -i "s/^worker_processes auto;/worker_processes $NGINX_PROCESSES;/g" /etc/nginx/nginx.conf
 		sed -i 's/\baccess_log[^;]*;/access_log \/dev\/stdout;/g' /etc/nginx/nginx.conf
 		sed -i 's/\berror_log[^;]*;/error_log \/dev\/stdout;/g' /etc/nginx/nginx.conf
 	fi
