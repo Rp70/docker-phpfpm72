1cdb312e30cf6dcd7fa871f55bf27a7aae196d26
Rp70 <14834373+Rp70@users.noreply.github.com>
2019-04-09 18:50:05 +0700

    commit

--- files/docker-start.sh
+++ files/docker-start.sh
@@ -2,96 +2,95 @@
 set -e
 
 
-if [ "$1" = 'startup' ]; then
-	CRON_ENABLE=${CRON_ENABLE:=''}
-	CRON_COMMANDS=${CRON_COMMANDS:=''}
-	MEMCACHED_ENABLE=${MEMCACHED_ENABLE:=''}
-	NGINX_ENABLE=${NGINX_ENABLE:=''}
-	NGINX_PROCESSES=${NGINX_PROCESSES:='2'}
-	NGINX_REALIP_FROM=${NGINX_REALIP_FROM:=''}
-	NGINX_REALIP_HEADER=${NGINX_REALIP_HEADER:=''}
-	#NGINX_DISABLE_MODULES=${NGINX_REALIP_HEADER:=''}
-	SUPERVISOR_ENABLE=0
+CRON_ENABLE=${CRON_ENABLE:=''}
+CRON_COMMANDS=${CRON_COMMANDS:=''}
+MEMCACHED_ENABLE=${MEMCACHED_ENABLE:=''}
+NGINX_ENABLE=${NGINX_ENABLE:=''}
+NGINX_PROCESSES=${NGINX_PROCESSES:='2'}
+NGINX_REALIP_FROM=${NGINX_REALIP_FROM:=''}
+NGINX_REALIP_HEADER=${NGINX_REALIP_HEADER:=''}
+#NGINX_DISABLE_MODULES=${NGINX_REALIP_HEADER:=''}
+SUPERVISOR_ENABLE=0
 
-	if [ "$CRON_COMMANDS" != '' ]; then
-		CRON_ENABLE="1"
-	fi
-	if [ "$CRON_ENABLE" = '' ]; then
-		rm -f /etc/supervisor/conf.d/cron.conf
-	else
-		SUPERVISOR_ENABLE=$((SUPERVISOR_ENABLE+1))
-	fi
+if [ "$CRON_COMMANDS" != '' ]; then
+	CRON_ENABLE="1"
+fi
+if [ "$CRON_ENABLE" = '' ]; then
+	rm -f /etc/supervisor/conf.d/cron.conf
+else
+	SUPERVISOR_ENABLE=$((SUPERVISOR_ENABLE+1))
+fi
 
-	if [ "$MEMCACHED_ENABLE" = '' ]; then
-		rm -f /etc/supervisor/conf.d/memcached.conf
-	else
-		SUPERVISOR_ENABLE=$((SUPERVISOR_ENABLE+1))
-	fi
+if [ "$MEMCACHED_ENABLE" = '' ]; then
+	rm -f /etc/supervisor/conf.d/memcached.conf
+else
+	SUPERVISOR_ENABLE=$((SUPERVISOR_ENABLE+1))
+fi
 
-	if [ "$NGINX_ENABLE" = '' ]; then
-		rm -f /etc/supervisor/conf.d/nginx.conf
-	else
-		SUPERVISOR_ENABLE=$((SUPERVISOR_ENABLE+1))
+if [ "$NGINX_ENABLE" = '' ]; then
+	rm -f /etc/supervisor/conf.d/nginx.conf
+else
+	SUPERVISOR_ENABLE=$((SUPERVISOR_ENABLE+1))
 
-		#rm -rf /etc/nginx/sites-available/default
-		#sed -i 's/^user/daemon off;\nuser/g' /etc/nginx/nginx.conf
-		#sed -i 's/^user www-data;/user coin;/g' /etc/nginx/nginx.conf
-		sed -i "s/^worker_processes auto;/worker_processes $NGINX_PROCESSES;/g" /etc/nginx/nginx.conf
-		#sed -i 's/\baccess_log[^;]*;/access_log \/dev\/stdout;/g' /etc/nginx/nginx.conf
-		#sed -i 's/\berror_log[^;]*;/error_log \/dev\/stdout;/g' /etc/nginx/nginx.conf
+	#rm -rf /etc/nginx/sites-available/default
+	#sed -i 's/^user/daemon off;\nuser/g' /etc/nginx/nginx.conf
+	#sed -i 's/^user www-data;/user coin;/g' /etc/nginx/nginx.conf
+	sed -i "s/^worker_processes auto;/worker_processes $NGINX_PROCESSES;/g" /etc/nginx/nginx.conf
+	#sed -i 's/\baccess_log[^;]*;/access_log \/dev\/stdout;/g' /etc/nginx/nginx.conf
+	#sed -i 's/\berror_log[^;]*;/error_log \/dev\/stdout;/g' /etc/nginx/nginx.conf
 
-		rm -rf /etc/nginx/modules-enabled/*.conf
-		
-		### realip_module ###
-		# Cloudflare IPv4: https://www.cloudflare.com/ips-v4
-		# Cloudflare IPv6: https://www.cloudflare.com/ips-v6
-		CONFFILE=/etc/nginx/conf.d/realip.conf
-		IPADDRS=""
-		for ipaddr in $NGINX_REALIP_FROM; do
-			if [ "$ipaddr" = "cloudflare" ]; then
-				IPADDRS="$IPADDRS `curl -f --connect-timeout 30 https://www.cloudflare.com/ips-v4 2> /dev/null`"
-				if [ $? -gt 0 ]; then
-					IPADDRS="$IPADDRS `cat /tmp/cloudflare-ips-v4 2> /dev/null`"
-				fi
-
-				IPADDRS="$IPADDRS `curl -f --connect-timeout 30 https://www.cloudflare.com/ips-v6 2> /dev/null`"
-				if [ $? -gt 0 ]; then
-					IPADDRS="$IPADDRS `cat /tmp/cloudflare-ips-v6 2> /dev/null`"
-				fi
-
-				NGINX_REALIP_HEADER='CF-Connecting-IP'
-			else
-				IPADDRS="$IPADDRS $ipaddr"
+	rm -rf /etc/nginx/modules-enabled/*.conf
+	
+	### realip_module ###
+	# Cloudflare IPv4: https://www.cloudflare.com/ips-v4
+	# Cloudflare IPv6: https://www.cloudflare.com/ips-v6
+	CONFFILE=/etc/nginx/conf.d/realip.conf
+	IPADDRS=""
+	for ipaddr in $NGINX_REALIP_FROM; do
+		if [ "$ipaddr" = "cloudflare" ]; then
+			IPADDRS="$IPADDRS `curl -f --connect-timeout 30 https://www.cloudflare.com/ips-v4 2> /dev/null`"
+			if [ $? -gt 0 ]; then
+				IPADDRS="$IPADDRS `cat /tmp/cloudflare-ips-v4 2> /dev/null`"
 			fi
-		done
 
-		if [ "$IPADDRS" != '' ]; then
-			echo "### This file is auto-generated. ###" > $CONFFILE
-			echo "### Your changes will be overwriten. ###" >> $CONFFILE
-			echo >> $CONFFILE
-			for ipaddr in $IPADDRS; do
-				echo "set_real_ip_from $ipaddr;" >> $CONFFILE
-			done
-			echo "real_ip_header $NGINX_REALIP_HEADER;" >> $CONFFILE
+			IPADDRS="$IPADDRS `curl -f --connect-timeout 30 https://www.cloudflare.com/ips-v6 2> /dev/null`"
+			if [ $? -gt 0 ]; then
+				IPADDRS="$IPADDRS `cat /tmp/cloudflare-ips-v6 2> /dev/null`"
+			fi
+
+			NGINX_REALIP_HEADER='CF-Connecting-IP'
+		else
+			IPADDRS="$IPADDRS $ipaddr"
 		fi
-		### / realip_module ###
+	done
+
+	if [ "$IPADDRS" != '' ]; then
+		echo "### This file is auto-generated. ###" > $CONFFILE
+		echo "### Your changes will be overwriten. ###" >> $CONFFILE
+		echo >> $CONFFILE
+		for ipaddr in $IPADDRS; do
+			echo "set_real_ip_from $ipaddr;" >> $CONFFILE
+		done
+		echo "real_ip_header $NGINX_REALIP_HEADER;" >> $CONFFILE
 	fi
+	### / realip_module ###
+fi
 
-	mkdir -p /var/log/php-fpm
-	touch /var/log/php-fpm/error.log
-	chown -R www-data:www-data /var/log/php-fpm
-	chmod 0777 /var/log/php-fpm
+mkdir -p /var/log/php-fpm
+touch /var/log/php-fpm/error.log
+chown -R www-data:www-data /var/log/php-fpm
+chmod 0777 /var/log/php-fpm
 
-	if [ -e /docker-start-hook.sh ]; then
-		. /docker-start-hook.sh
-	fi
+if [ -e /docker-start-hook.sh ]; then
+	. /docker-start-hook.sh
+fi
 
+if [ "$1" = 'startup' ]; then
 	if [ "$SUPERVISOR_ENABLE" -gt 0 ]; then
 		exec supervisord --nodaemon;
 	else
 		exec php-fpm --nodaemonize;
 	fi
-	
 else
 	exec "$@"
 fi
